271bd95fe7a0edd4e482b9f4c79fb4ce6d45eb43
oyatillo0509 <tilloxon0509@gmail.com>
2024-05-29 12:56:53 +0500

    finished

---
+++ js/index.js
@@ -0,0 +1,66 @@
+// 1-masala
+// function hisobla(t, T) {
+//   return (t + T) % 24;
+// }
+// let t = 9;
+// let T = 48;
+// let ketish = hisobla(t, T);
+// console.log(ketish);
+
+// 2-masala
+// function K(c) {
+//   return c + 273.15;
+// }
+
+// function F(c) {
+//   return c * 1.8 + 32;
+// }
+
+// let c = 32;
+// let k = K(c);
+// let f = F(c);
+
+// console.log("Kelvin: " + k);
+// console.log("Fahrenheit: " + f);
+
+// 3-masala
+// XXXX;
+
+// 4-masala
+
+// let n = +prompt("og'irligini kiriting");
+// let a = +prompt("foizini kiriting");
+// let b = +prompt("% qiymat");
+
+// let res = (((n * a) / 100) * b) / 100;
+
+// console.log(res);
+
+// 5-masala
+// let n = +prompt("sonini kiriting");
+// let deg = 180 * (n - 1);
+// console.log(deg);
+
+// 6-masala
+// let soat = +prompt("Soatni kiriting");
+// let minut = +prompt("Minutni kiriting");
+// let add_minut = +prompt("+ minutni kiriting:");
+
+// minut += add_minut;
+// soat = (soat + Math.floor(minut / 60)) % 24;
+// minut %= 60;
+
+// console.log(soat + " : " + minut);
+
+// 7-masala
+// xxxxxx
+
+// 8-masala
+// let son = +prompt("xodimlar soni");
+// let yig = 0;
+
+// for (let i = 0; i < son; i++) {
+//   yig += +prompt(`Xodim ${i + 1} maoshini kiriting:`);
+// }
+
+// console.log("o'rtacha maosh " + yig  / son);
